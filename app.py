3607b20865298af149e5d4a139e70833a491d2f0
mnagarale <32774496+mnagarale@users.noreply.github.com>
2020-03-01 11:13:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 print (" Hello World")
-some random lines
+
 print (" New line" ) 
 print ( "This is the new line" )
 name= 'manoj'
 print ( name )
   
 print ( " Line added by DEV branch" )
+print ( " To test Jenkins" )
